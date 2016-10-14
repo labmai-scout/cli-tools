c85737a70e1b1b1899a462ea91a42ba617eabe12
pihizi <pihizi@msn.com>
2016-10-14 10:31:52 +0000

    .

--- labmai-runtime/start.sh
+++ labmai-runtime/start.sh
@@ -1,22 +1,5 @@
 #/bin/bash
 
-echo "\n\n"
-echo "环境将被搭建在/data目录下，请确保目录存在且为空"
-echo "\n\n"
-
-export node=$1
-export currentPath=$PWD
-export envROOT="/data"
-export shellPath="$currentPath/shell"
-export containersPath="$currentPath/containers"
-export dockerIP=`ifconfig docker0 | grep 'inet addr' | awk -F: '{ print $2 }' | awk -F\  '{ print $1 }'`
-
-if [ "$node" == "" ];
-then
-    echo "请输入你希望操作的node，如node: $0 node"
-    exit
-fi
-
 function confirm() {
     echo "$1 (Y/n): "
     read tmpNeed
@@ -27,12 +10,29 @@ function confirm() {
     return 0
 }
 
-shellFiles=`ls -v $shellPath`
-for shellFile in $shellFiles
-do
-    if [[ $shellFile == *\.sh ]]
-    then
-        source $shellPath/$shellFile
-    fi
-done
+echo "- 请确保使用admin权限执行该脚本"
+echo "- 环境将被搭建在/data目录下，请确保目录存在且为空"
 
+confirm "继续" && {
+    export node=$1
+    export currentPath=$PWD
+    export envROOT="/data"
+    export shellPath="$currentPath/shell"
+    export containersPath="$currentPath/containers"
+    export dockerIP=`ifconfig docker0 | grep 'inet addr' | awk -F: '{ print $2 }' | awk -F\  '{ print $1 }'`
+
+    if [ "$node" == "" ];
+    then
+        echo "请输入你希望操作的node，如node: $0 node"
+        exit
+    fi
+
+    shellFiles=`ls -v $shellPath`
+    for shellFile in $shellFiles
+    do
+        if [[ $shellFile == *\.sh ]]
+        then
+            source $shellPath/$shellFile
+        fi
+    done
+}
